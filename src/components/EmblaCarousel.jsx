55f8e09063f680ba2dc1cf9e388c41fb33c32e31
Ataur Rahaman <ataurfb4445@gmail.com>
2025-09-03 18:05:11 +0600

    added React-simple-typewriter to make text appearance good

--- src/components/EmblaCarousel.jsx
+++ src/components/EmblaCarousel.jsx
@@ -2,6 +2,7 @@ import React, { use } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import { ThemeContext } from '../theme/ThemeContext';
+import { Typewriter } from 'react-simple-typewriter';
 
 export function EmblaCarousel() {
   const {theme} = use(ThemeContext);
@@ -9,7 +10,19 @@ export function EmblaCarousel() {
 
   return (
     <div className={`flex flex-col items-center justify-center py-[50px] md:py-[100px] px-2 ${theme === "light" ? "bg-[#E7F6FD]" : "bg-gray-900"}`}>
-        <h1 className={`text-4xl ${theme === "light" ? "text-black" : "text-white"} text-center md:text-5xl font-extrabold leading-tight tracking-tight mb-[50px]`}>Find a Roommate Who <span className='text-[#0EA5E9]'>Fits Your Life</span></h1>
+        <h1 className={`text-4xl ${theme === "light" ? "text-black" : "text-white"} text-center md:text-5xl font-extrabold leading-tight tracking-tight mb-[50px]`}>Find a Roommate Who - <span style={{ color: '#0EA5E9', fontWeight: 'bold' }}>
+                  
+                  <Typewriter
+                    words={['Fits Your Life', 'is responsible', 'is polite', 'is super cool']}
+                    loop={30}
+                    cursor
+                    cursorStyle='>'
+                    typeSpeed={100}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                  />
+                </span>
+        </h1>
         <div className="embla max-w-7xl rounded-3xl shadow-xl cursor-grab" ref={emblaRef}>
         <div className="embla__container rounded-3xl">
             <div className="embla__slide"><img className='rounded-3xl' src="https://i.ibb.co.com/pBtVpVR8/banner1.webp" alt="banner-1" /></div>
--- src/components/FeaturedRoommate.jsx
+++ src/components/FeaturedRoommate.jsx
@@ -1,6 +1,7 @@
 import React, { use } from "react";
 import FeaturedCard from "./FeaturedCard";
 import { ThemeContext } from "../theme/ThemeContext";
+import { Typewriter } from 'react-simple-typewriter'
 
 const FeaturedRoommate = ({ featuredData }) => {
   const {theme} = use(ThemeContext);
@@ -9,7 +10,19 @@ const FeaturedRoommate = ({ featuredData }) => {
     <div className={theme === "light" ? "bg-white" : "bg-gray-700"}>
     <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-20 lg:py-24 ${theme === "light" ? "bg-white" : "bg-gray-700"}`}>
   <h1 className={`text-3xl ${theme === "light" ? "text-black" : "text-white"} sm:text-4xl md:text-5xl font-extrabold text-center leading-tight tracking-tight mb-10 sm:mb-14`}>
-    Recommended <span className="text-[#0EA5E9]">For You</span>
+    Recommended <span className="text-[#0EA5E9]">For You - </span>
+    <span style={{ color: '#0EA5E9', fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['Best-match', 'Better-home', 'Good-vibe']}
+            loop={30}
+            cursor
+            cursorStyle='>'
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
   </h1>
 
   <div className="grid gap-6 sm:gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
